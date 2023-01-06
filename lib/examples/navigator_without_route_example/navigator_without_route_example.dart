843a3442e2da72f9fb0c55d016b367f89971c396
DJSeokHo <djseokho@vip.qq.com>
2023-01-06 23:06:48 +0900

    SeokHo

---
+++ lib/examples/navigator_without_route_example/navigator_without_route_example.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class NavigatorWithoutRouteExample extends StatelessWidget {
+  const NavigatorWithoutRouteExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: _ContentView(),
+    );
+  }
+}
+
+class _ContentView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.black,
+        width: double.infinity,
+        height: double.infinity,
+        child: const Center(
+          child: Text(
+            "Hello"
+          ),
+        ),
+      ),
+    );
+  }
+
+}
