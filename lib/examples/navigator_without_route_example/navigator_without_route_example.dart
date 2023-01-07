1f02c836aa5301a0627ac1d9e60241a4ea36730e
DJSeokHo <djseokho@vip.qq.com>
2023-01-07 20:37:50 +0900

    SeokHo

--- lib/examples/navigator_without_route_example/navigator_without_route_example.dart
+++ lib/examples/navigator_without_route_example/navigator_without_route_example.dart
@@ -17,17 +17,87 @@ class _ContentView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("AppBar"),
+        centerTitle: true, // 无论在什么机器上，标题都居中显示
+        backgroundColor: Colors.deepOrange,
+        // leading: const Icon(Icons.menu), // Icon 无法点击
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.next_plan),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => _SubView(),
+                    fullscreenDialog: false
+                ),
+              );
+            },
+          ),
+        ],
+      ),
       body: Container(
         color: Colors.black,
         width: double.infinity,
         height: double.infinity,
-        child: const Center(
-          child: Text(
-            "Hello",
-            style: TextStyle(
-              color: Colors.white
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              "Hello",
+              style: TextStyle(
+                  color: Colors.white
+              )
             ),
-          ),
+            ElevatedButton(
+              onPressed: () => {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => _SubView(),
+                    fullscreenDialog: false
+                  ),
+                )
+              }, child: const Text(
+                "next"
+              )
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _SubView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.black,
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+                "Hello",
+                style: TextStyle(
+                    color: Colors.white
+                )
+            ),
+            ElevatedButton(
+                onPressed: () => {
+                  Navigator.pop(context)
+                }, child: const Text(
+                "back"
+            )
+            )
+          ],
         ),
       ),
     );
